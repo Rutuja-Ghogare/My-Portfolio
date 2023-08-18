69fc160d2a6ef4df9fd12db64d36ed95276b71c5
ruutujaa <ghogare.r@northeastern.edu>
2023-08-18 13:03:22 -0400

    created a portfolio using NextJs and TailwindCSS

---
+++ components/AboutSection.tsx
@@ -0,0 +1,94 @@
+import React from "react"
+import Image from "next/image"
+
+const skills = [
+  { skill: "HTML" },
+  { skill: "CSS" },
+  { skill: "JavaScript" },
+  { skill: "TypeScript" },
+  { skill: "Python" },
+  { skill: "Java" },
+  { skill: "React" },
+  { skill: "Next.js" },
+  { skill: "Tailwind CSS" },
+  { skill: "Git" },
+  { skill: "GitHub" },
+  { skill: "Jupyter Notebook" },
+]
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-600 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+              Get to know me!
+            </h1>
+            <p>
+              Hi, my name is Rutuja and I am a{" "}
+              <span className="font-bold">{"highly ambitious"}</span>,
+              <span className="font-bold">{" self-motivated"}</span>, and
+              <span className="font-bold">{" driven"}</span> Full Stack Developer.
+            </p>
+            <br />
+            <p>
+            I am currently enrolled in a Master of Science in Information
+             Systems program at Northeastern University, Boston.
+            I am an enthusiastic Computer Engineer who enjoys problem solving in Software Engineering
+             and User Experience. I have practiced programming in many different languages such as C, C++, JavaScript
+              and Python. Apart from this I'm more interested in Front end Development and UI/UX design.
+            </p>
+            <br />
+            <p>
+            I have completed my Undergraduation from Pune University with a (8.71/10 GPA) in Computer Science.
+            I've been constantly involved in developing scalable and relevant projects for my coursework and personal interests, 
+            and I hope to use my skill set to reinvent techniques and specialize in software and front end development to deliver 
+            high-quality products.
+            </p>
+            <br />
+            <p>
+              I believe that you should{" "}
+              <span className="font-bold text-teal-500">
+                never stop growing
+              </span>{" "}
+              and that&#39;s what I strive to do, I have a passion for
+              technology and a desire to always push the limits of what is
+              possible. I am excited to see where my career takes me and am
+              always open to new opportunities. 🙂
+            </p>
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                )
+              })}
+            </div>
+            <Image
+              src="/hero-image.png"
+              alt=""
+              width={325}
+              height={325}
+              className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
---
+++ components/ProjectsSection.tsx
@@ -0,0 +1,104 @@
+import React from "react"
+import Image from "next/image"
+import Link from "next/link"
+import SlideUp from "./SlideUp"
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
+
+const projects = [
+  {
+    name: "Trader Joe's",
+    description:
+      "•	Developed a full-stack web application for Trader Joe’s using the MERN stack",
+    image: "/Traderjoes.png",
+    github: "https://github.com/AditiChikkali/Ecommerce_Website",
+    link: "https://ecommerce-jg5e.onrender.com",
+  },
+  {
+    name: "A Byte of yummy",
+    description: "A food application using API that helps you to get instructions for every recipe you want and the list of ingredients as well",
+    image: "/food.png",
+    github: "https://github.com/Rutuja1805/FoodRecipeApp",
+    link: "https://food-recipe-app-gold.vercel.app/",
+  },
+  {
+    name: "CocktailQuest",
+    description: "CocktailQuest is a dynamic web app that empowers users to explore a vast array of cocktails by offering a robust search feature and detailed information including categories, ingredients, and preparation instructions.",
+    image: "/cocktail.png",
+    github: "https://github.com/Rutuja1805/CocktailQuest",
+    link: "https://cocktail-quest-nine.vercel.app/",
+  },
+  {
+    name: "HealthPlus Services",
+    description:
+      "Health Plus would act as a bridge between enterprises and global members. Users will be able to offer services and easily connect to medical support. Health Plus also keeps track of the data and various types of service roles.",
+    image: "/health.png",
+    github: "https://github.com/Rutuja1805/HealthPlus_Services",
+    link: "https://github.com/Rutuja1805/HealthPlus_Services",
+  },
+  {
+    name: "Rutuja's To-do List",
+    description:
+      "A To-do list using react for managing our daily tasks.",
+    image: "/todo.png",
+    github: "https://github.com/Rutuja1805/TODO_List",
+    link: "https://todo-list-82vwc1zmi-rutuja1805.vercel.app/",
+  },
+]
+
+const ProjectsSection = () => {
+  return (
+    <section id="projects">
+      <h1 className="my-10 text-center font-bold text-4xl">
+        Projects
+        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+      </h1>
+
+      <div className="flex flex-col space-y-28">
+        {projects.map((project, idx) => {
+          return (
+            <div key={idx}>
+              <SlideUp offset="-300px 0px -300px 0px">
+                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                  <div className=" md:w-1/2">
+                    <Link href={project.link}>
+                      <Image
+                        src={project.image}
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        className="rounded-xl shadow-xl hover:opacity-70"
+                      />
+                    </Link>
+                  </div>
+                  <div className="mt-8 md:w-1/2">
+                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                      {project.description}
+                    </p>
+                    <div className="flex flex-row align-bottom space-x-4">
+                      <Link href={project.github} target="_blank">
+                        <BsGithub
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                      <Link href={project.link} target="_blank">
+                        <BsArrowUpRightSquare
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </SlideUp>
+            </div>
+          )
+        })}
+        
+      </div>
+    </section>
+  )
+}
+
+export default ProjectsSection
