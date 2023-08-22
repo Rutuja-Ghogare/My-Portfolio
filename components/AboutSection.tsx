0baacc61af4338231e25f8d49ebfcd879c64d5e3
ruutujaa <ghogare.r@northeastern.edu>
2023-08-22 00:35:03 -0400

    updated about section

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -22,34 +22,34 @@ const AboutSection = () => {
       <div className="my-12 pb-12 md:pt-16 md:pb-48">
         <h1 className="text-center font-bold text-4xl">
           About Me
-          <hr className="w-6 h-1 mx-auto my-4 bg-teal-600 border-0 rounded"></hr>
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-600 border-0 rounded" />
         </h1>
 
         <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
-          <div className="md:w-1/2 ">
+          <div className="md:w-1/2">
             <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
               Get to know me!
             </h1>
             <p>
               Hi, my name is Rutuja and I am a{" "}
-              <span className="font-bold">{"highly ambitious"}</span>,
-              <span className="font-bold">{" self-motivated"}</span>, and
-              <span className="font-bold">{" driven"}</span> Full Stack Developer.
+              <span className="font-bold">&apos;highly ambitious&apos;</span>,
+              <span className="font-bold">&apos;self-motivated&apos;</span>, and
+              <span className="font-bold">&apos;driven&apos;</span> Full Stack Developer.
             </p>
             <br />
             <p>
-            I am currently enrolled in a Master of Science in Information
-             Systems program at Northeastern University, Boston.
-            I am an enthusiastic Computer Engineer who enjoys problem solving in Software Engineering
-             and User Experience. I have practiced programming in many different languages such as C, C++, JavaScript
-              and Python. Apart from this I'm more interested in Front end Development and UI/UX design.
+              I am currently enrolled in a Master of Science in Information
+              Systems program at Northeastern University, Boston.
+              I am an enthusiastic Computer Engineer who enjoys problem solving in Software Engineering
+              and User Experience. I have practiced programming in many different languages such as C, C++, JavaScript
+              and Python. Apart from this I&apos;m more interested in Front end Development and UI/UX design.
             </p>
             <br />
             <p>
-            I have completed my Undergraduation from Pune University with a (8.71/10 GPA) in Computer Science.
-            I've been constantly involved in developing scalable and relevant projects for my coursework and personal interests, 
-            and I hope to use my skill set to reinvent techniques and specialize in software and front end development to deliver 
-            high-quality products.
+              I have completed my Undergraduation from Pune University with a (8.71/10 GPA) in Computer Science.
+              I&apos;ve been constantly involved in developing scalable and relevant projects for my coursework and personal interests, 
+              and I hope to use my skill set to reinvent techniques and specialize in software and front end development to deliver 
+              high-quality products.
             </p>
             <br />
             <p>
@@ -57,7 +57,7 @@ const AboutSection = () => {
               <span className="font-bold text-teal-500">
                 never stop growing
               </span>{" "}
-              and that&#39;s what I strive to do, I have a passion for
+              and that&apos;s what I strive to do, I have a passion for
               technology and a desire to always push the limits of what is
               possible. I am excited to see where my career takes me and am
               always open to new opportunities. 🙂
