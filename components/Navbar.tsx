5aee25875511840511ae7323be2d9d46b7ab17d6
ruutujaa <ghogare.r@northeastern.edu>
2023-09-23 22:55:02 -0400

    changed HeroSection and Resume

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,8 @@
 "use client" // this is a client component
 import React from "react"
 import { useState } from "react"
-import { Link } from "react-scroll/modules"
+// import { Link } from "react-scroll/modules"
+import { Link, animateScroll as scroll } from "react-scroll"
 import { usePathname } from "next/navigation"
 import { useTheme } from "next-themes"
 import { RiMoonFill, RiSunLine } from "react-icons/ri"
